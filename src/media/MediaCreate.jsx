d7fdd36c8ce4008d730d78978a49ec352787ccc5
Amber Janzen <janzenamber@gmail.com>
2020-09-14 16:24:39 -0400

    tweaks to actions and creat media

--- src/media/MediaCreate.jsx
+++ src/media/MediaCreate.jsx
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react'
-import {Button, Form, FormGroup, Label, Input} from 'reactstrap';
+import React, {useState} from 'react'
+import {Button, Form, FormGroup, Label, Input, UncontrolledCollapse, Card, CardBody} from 'reactstrap';
 
 const MediaCreate = (props) => {
 
@@ -11,6 +11,8 @@ const MediaCreate = (props) => {
     const [rating, setRating] = useState('');
     const [consumed, setConsumed] = useState('');
     const [platform, setPlatform] = useState('');
+
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -45,10 +47,11 @@ const MediaCreate = (props) => {
 
         })
     }
+    
     return(
         <div>
             <h3>Add to your Media Collection</h3>
-            <Form onSubmit = {handleSubmit}>
+            <Form id="create" onSubmit = {handleSubmit}>
                 <FormGroup>
                     <Label htmlFor="type"> Type: </Label>
                     <Input type="select"name="type" value ={type} onChange ={(e) => setType (e.target.value)}>
