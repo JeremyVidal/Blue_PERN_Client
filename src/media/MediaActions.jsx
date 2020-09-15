34a45d943b82d88fe716caf33f9cb58dd8e0368d
Amber Janzen <janzenamber@gmail.com>
2020-09-15 08:28:09 -0400

    minor tweaks

--- src/media/MediaActions.jsx
+++ src/media/MediaActions.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import MediaCreate from './MediaCreate';
 import {
     Button,
@@ -8,6 +8,19 @@ import {
 // import MediaEdit from './MediaEdit';
 
 const MediaActions = () => {
+    const [sessionToken, setSessionToken] = useState(undefined);
+    console.log(sessionToken);
+
+    useEffect(() => console.log('session token has changed'), [sessionToken]);
+
+    const viewConductor = () => {
+        return sessionToken ?
+        <media sessionToken={sessionToken} /> :
+        <auth
+         message= "test"
+         setSessionToken={setSessionToken}
+        />
+    }
 
 return (
     <div>
@@ -15,7 +28,7 @@ return (
         <h1>Welcome *user*</h1>
         <Col md ="6">
         <h3>Select Media Type</h3>
-        <MediaCreate />
+        <MediaCreate setSessionToken={setSessionToken}/>
         </Col>
         <Col md ="3">
             <h3>workout edit</h3>
@@ -23,6 +36,7 @@ return (
         </Col>
         <Button /*onClick={MediaTable}*/>Go to your Collection</Button>
         </Row>
+        {viewConductor()}
     </div>
 )
 
